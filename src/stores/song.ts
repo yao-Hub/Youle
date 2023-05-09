21776a8f63a379f5b371e6ca076593059a7e7ee9
yzy01452572 <yzy01452572@alibaba-inc.com>
2023-05-09 09:33:01 +0800

    pina 当前音乐及播放暂停控制

--- src/stores/song.ts
+++ src/stores/song.ts
@@ -1,65 +1,100 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
 import { ref, reactive } from 'vue';
 
-interface state {
-  musiclist: musiclist
+
+// 第一种写法
+
+// interface state {
+//   musiclist: musiclist
+// }
+
+// musiclist第一种定义方法：A
+
+// interface musiclist {
+//   [index: number]: {
+//     id: string | number;
+//     name: string;
+//     [key: string]: any
+//   }
+// }
+
+// export const songStore = defineStore('song', () => {
+//   // 当前播放的音乐id
+//   const playSongId = ref<number | string>(0)
+
+//   const play = ref<boolean>(false)
+
+//   const state = reactive<state>({
+//     musiclist: [{
+//       id: 1,
+//       name: '音乐1'
+//     }]
+//   });
+
+//   function changePlay(ifPlay?: boolean) {
+//     play.value = ifPlay ? ifPlay : !play.value
+//   }
+
+//   function changeCurrentSong(id: string | number) {
+//     playSongId.value = id
+//   }
+//   return { play, playSongId, changePlay, changeCurrentSong, musiclist: state.musiclist }
+// })
+
+// musiclist第二种定义方法：B
+interface Music {
+  id: number;
+  name: string;
+  url?: string;
 }
 
-interface musiclist {
-  [index: number]: {
-    id: string | number;
-    name: string;
-    [key: string]: any
-  }
-}
+// // musiclist第三种定义方法：C
+// type MusicList = Record<number, {id: number, name: string, url?:string}>
 
-export const songStore = defineStore('song', () => {
-  // 当前播放的音乐id
-  const playSongId = ref<number | string>(0)
+// 第二种写法
+export const songStore = defineStore({
+  id: 'song',
+  state: () => ({
+    playSongId: <number | string>0,
+    play: <boolean>false,
 
-  const play = ref<boolean>(false)
+    // A
+    // musiclist: <musiclist>[{
+    //   id: 1,
+    //   name: '音乐1'
+    // },{
+    //   id: 2,
+    //   name: '音乐名2',
+    //   url: 'url',
+    // }]
 
-  const state = reactive<state>({
-    musiclist: [{
+    // B
+    musiclist:<Music []> [{
       id: 1,
-      name: '音乐1'
-    },{
-      id: 2,
-      name: '音乐名2',
-      url: 'url',
-    },{
-      id: 3,
-      name: '音乐名3',
-      url: 'url',
-    },{
-      id: 4,
-      name: '音乐名4',
-      url: 'url',
-    },{
-      id: 5,
-      name: '音乐名5',
-      url: 'url',
-    },{
-      id: 6,
-      name: '音乐名6',
-      url: 'url',
-    },{
-      id: 7,
-      name: '音乐名7',
-      url: 'url',
-    },{
-      id: 8,
-      name: '音乐名8',
+      name: '音乐1',
       url: 'url',
     }]
-  });
 
-  function changePlay(ifPlay?: boolean) {
-    play.value = ifPlay ? ifPlay : !play.value
+    // C
+    // musiclist: <MusicList>{
+    //   1: { id: 1, name: '音乐1'},
+    //   2: { id: 2, name: '音乐名2', url: 'url' },
+    // }
+  }),
+  actions: {
+    changePlay(ifPlay?: boolean) {
+      this.play = ifPlay ? ifPlay : !this.play
+    },
+    changeCurrentSong(id: string | number) {
+      this.playSongId = id
+    }
   }
-
-  function changeCurrentSong(id: string | number) {
-    playSongId.value = id
-  }
-  return { play, playSongId, changePlay, changeCurrentSong, musiclist: state.musiclist }
 })
+
+interface extImportMeta extends ImportMeta {
+  hot?: any
+}
+const extIMeta: extImportMeta = import.meta;
+if (extIMeta.hot) {
+  extIMeta.hot.accept(acceptHMRUpdate(songStore, extIMeta.hot));
+}
